96fc2f1fb8491bd229fd155fef77af659352c052
nicholas hung <nhung93@outlook.com>
2022-05-15 15:28:41 -0400

    fix spacing

--- src/PdfManager.js
+++ src/PdfManager.js
@@ -5,150 +5,150 @@
 
 class PdfManager {
 
-	constructor(updateStateFunc){
-		this.pdfDoc = null;
-		this.pageNum = 1;
-		this.pageRendering = false;
-		this.pageNumPending = null;
-		this.scale = 1.0;
-		this.canvas = null;
-		this.ctx = null;
-		
-		// use this function to update the state of ScoreDisplay
-		this.updateUiState = updateStateFunc;
-		
-		// Loaded via <script> tag, create shortcut to access PDF.js exports.
-		this.pdfjsLib = window['pdfjs-dist/build/pdf'];
+    constructor(updateStateFunc){
+        this.pdfDoc = null;
+        this.pageNum = 1;
+        this.pageRendering = false;
+        this.pageNumPending = null;
+        this.scale = 1.0;
+        this.canvas = null;
+        this.ctx = null;
+        
+        // use this function to update the state of ScoreDisplay
+        this.updateUiState = updateStateFunc;
+        
+        // Loaded via <script> tag, create shortcut to access PDF.js exports.
+        this.pdfjsLib = window['pdfjs-dist/build/pdf'];
 
-		// The workerSrc property shall be specified.
-		this.pdfjsLib.GlobalWorkerOptions.workerSrc = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.worker.min.js";
-	}
-	
-	setCanvas(canvasElement){
-		this.canvas = canvasElement;
-		this.ctx = this.canvas.getContext('2d');
-	}
+        // The workerSrc property shall be specified.
+        this.pdfjsLib.GlobalWorkerOptions.workerSrc = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.worker.min.js";
+    }
+    
+    setCanvas(canvasElement){
+        this.canvas = canvasElement;
+        this.ctx = this.canvas.getContext('2d');
+    }
 
-	/**
-	 * Get page info from document, resize canvas accordingly, and render page.
-	 * @param num Page number.
-	 */
-	renderPage(num) {
-	  this.pageRendering = true;
-	  
-	  // Using promise to fetch the page
-	  this.pdfDoc.getPage(num).then((page) => {
-		let viewport = page.getViewport({scale: 1});
-		
-		let scale = this.canvas.parentNode.clientWidth / viewport.width; // try to make canvas responsive to client viewport
-		scale = scale > 1 ? 1.0 : scale; // don't let it exceed 1
-		
-		viewport = page.getViewport({scale: scale});
-		this.canvas.height = viewport.height;
-		this.canvas.width = viewport.width;
+    /**
+     * Get page info from document, resize canvas accordingly, and render page.
+     * @param num Page number.
+     */
+    renderPage(num) {
+      this.pageRendering = true;
+      
+      // Using promise to fetch the page
+      this.pdfDoc.getPage(num).then((page) => {
+        let viewport = page.getViewport({scale: 1});
+        
+        let scale = this.canvas.parentNode.clientWidth / viewport.width; // try to make canvas responsive to client viewport
+        scale = scale > 1 ? 1.0 : scale; // don't let it exceed 1
+        
+        viewport = page.getViewport({scale: scale});
+        this.canvas.height = viewport.height;
+        this.canvas.width = viewport.width;
 
-		// Render PDF page into canvas context
-		let renderContext = {
-		  canvasContext: this.ctx,
-		  viewport: viewport
-		};
-		let renderTask = page.render(renderContext);
+        // Render PDF page into canvas context
+        let renderContext = {
+          canvasContext: this.ctx,
+          viewport: viewport
+        };
+        let renderTask = page.render(renderContext);
 
-		// Wait for rendering to finish
-		renderTask.promise.then(() => {
-		  this.pageRendering = false;
-		  this.pageNum = num;
-		  
-		  // Update page counters
-		  this.updateUiState({
-			  "currPage": num,
-		  });
-	  
-		  if (this.pageNumPending !== null) {
-			// New page rendering is pending
-			this.renderPage(this.pageNumPending);
-			this.pageNumPending = null;
-		  }
-		});
-	  });
+        // Wait for rendering to finish
+        renderTask.promise.then(() => {
+          this.pageRendering = false;
+          this.pageNum = num;
+          
+          // Update page counters
+          this.updateUiState({
+              "currPage": num,
+          });
+      
+          if (this.pageNumPending !== null) {
+            // New page rendering is pending
+            this.renderPage(this.pageNumPending);
+            this.pageNumPending = null;
+          }
+        });
+      });
 
-	}
+    }
 
-	/**
-	 * If another page rendering in progress, waits until the rendering is
-	 * finished. Otherwise, executes rendering immediately.
-	 */
-	queueRenderPage(num) {
-	  if (this.pageRendering) {
-		this.pageNumPending = num;
-	  } else {
-		this.renderPage(num);
-	  }
-	}
+    /**
+     * If another page rendering in progress, waits until the rendering is
+     * finished. Otherwise, executes rendering immediately.
+     */
+    queueRenderPage(num) {
+      if (this.pageRendering) {
+        this.pageNumPending = num;
+      } else {
+        this.renderPage(num);
+      }
+    }
 
-	/**
-	 * Displays previous page.
-	*/
-	onPrevPage() {
-	  if (this.pageNum <= 1) {
-		return;
-	  }
-	  this.pageNum--;
-	  this.queueRenderPage(this.pageNum);
-	}
+    /**
+     * Displays previous page.
+    */
+    onPrevPage() {
+      if (this.pageNum <= 1) {
+        return;
+      }
+      this.pageNum--;
+      this.queueRenderPage(this.pageNum);
+    }
 
-	/**
-	 * Displays next page.
-	*/
-	onNextPage() {
-	  if (this.pageNum >= this.pdfDoc.numPages) {
-		return;
-	  }
-	  this.pageNum++;
-	  this.queueRenderPage(this.pageNum);
-	}
+    /**
+     * Displays next page.
+    */
+    onNextPage() {
+      if (this.pageNum >= this.pdfDoc.numPages) {
+        return;
+      }
+      this.pageNum++;
+      this.queueRenderPage(this.pageNum);
+    }
 
-	/**
-	 * Asynchronously downloads PDF.
-	 
-	pdfjsLib.getDocument(url).promise.then(function(pdfDoc_) {
-	  pdfDoc = pdfDoc_;
-	  document.getElementById('page_count').textContent = pdfDoc.numPages;
+    /**
+     * Asynchronously downloads PDF.
+     
+    pdfjsLib.getDocument(url).promise.then(function(pdfDoc_) {
+      pdfDoc = pdfDoc_;
+      document.getElementById('page_count').textContent = pdfDoc.numPages;
 
-	  // Initial/first page rendering
-	  renderPage(pageNum);
-	});
+      // Initial/first page rendering
+      renderPage(pageNum);
+    });
 
-	*/
+    */
 
-	findScorePage(time, timeMarkers){
-		// given time in seconds and a map of pages to times (in sec),
-		// find what the page should be at that time.
-		let pageToBeOn = parseInt(Object.keys(timeMarkers)[0]);
-		for(let page in timeMarkers){
-			if(time <= timeMarkers[page]){
-				pageToBeOn = parseInt(page);
-				break;
-			}
-		}
-		return pageToBeOn;
-	}
+    findScorePage(time, timeMarkers){
+        // given time in seconds and a map of pages to times (in sec),
+        // find what the page should be at that time.
+        let pageToBeOn = parseInt(Object.keys(timeMarkers)[0]);
+        for(let page in timeMarkers){
+            if(time <= timeMarkers[page]){
+                pageToBeOn = parseInt(page);
+                break;
+            }
+        }
+        return pageToBeOn;
+    }
 
-	async loadScore(scorePath, pageToRenderInitially){
-		return this.pdfjsLib.getDocument(scorePath).promise.then((pdfDoc_) => {
-		  this.pdfDoc = pdfDoc_;
-		  
-		  this.updateUiState({"totalPages": this.pdfDoc.numPages});
+    async loadScore(scorePath, pageToRenderInitially){
+        return this.pdfjsLib.getDocument(scorePath).promise.then((pdfDoc_) => {
+          this.pdfDoc = pdfDoc_;
+          
+          this.updateUiState({"totalPages": this.pdfDoc.numPages});
 
-		  // Initial/first page rendering
-		  this.pageNum = pageToRenderInitially;
-		  this.renderPage(pageToRenderInitially);
-		  
-		  return true;
-		});
-	}
+          // Initial/first page rendering
+          this.pageNum = pageToRenderInitially;
+          this.renderPage(pageToRenderInitially);
+          
+          return true;
+        });
+    }
 }
 
 export {
-	PdfManager
+    PdfManager
 }
--- src/ScoreRouter.js
+++ src/ScoreRouter.js
@@ -10,41 +10,41 @@ import {
 import { ScoreDisplay } from "./ScoreDisplay.js";
 
 const ScoreRouter = (props) => {
-	const audioManager = props.audioManager;
-	const pdfManager = props.pdfManager;
-	
-	const [currScoreCategories, setScoreCategories] = useState({});
+    const audioManager = props.audioManager;
+    const pdfManager = props.pdfManager;
+    
+    const [currScoreCategories, setScoreCategories] = useState({});
     const [currAboutState, setAboutState] = useState(false);
-	const [currMenuState, setMenuState] = useState("hide menu");
+    const [currMenuState, setMenuState] = useState("hide menu");
     
     function toggleAbout(){
         setAboutState(!currAboutState);
     }
-	
-	useEffect(() => {
-		async function getScoreNames(){
-			let res = await fetch('src/scoreNames.json');
-			res = await res.json();
-			setScoreCategories(res);
-		}
-		getScoreNames();
-	}, []);
-	
-	return (
-		<Router>
-			<div id='toggleSidebar'>
-				<button 
-					className="toggleSidebarBtn" 
-					onClick={() => {
-						if(currMenuState === "show menu"){
-							setMenuState("hide menu");
-						}else{
-							setMenuState("show menu");
-						}
-					}}
-				>{currMenuState}</button>
-			</div>
-			<nav className={currMenuState === "hide menu" ? 'naviOn' : 'naviOff'}>
+    
+    useEffect(() => {
+        async function getScoreNames(){
+            let res = await fetch('src/scoreNames.json');
+            res = await res.json();
+            setScoreCategories(res);
+        }
+        getScoreNames();
+    }, []);
+    
+    return (
+        <Router>
+            <div id='toggleSidebar'>
+                <button 
+                    className="toggleSidebarBtn" 
+                    onClick={() => {
+                        if(currMenuState === "show menu"){
+                            setMenuState("hide menu");
+                        }else{
+                            setMenuState("show menu");
+                        }
+                    }}
+                >{currMenuState}</button>
+            </div>
+            <nav className={currMenuState === "hide menu" ? 'naviOn' : 'naviOff'}>
                 <h2 className="aboutHeader" onClick={toggleAbout}>about</h2>
                 {
                     currAboutState &&
@@ -55,51 +55,51 @@ const ScoreRouter = (props) => {
                     </React.Fragment>
                 }
                 <hr />
-				<h2> score list </h2>
-				<hr />
-				<ul>
-				{
-					Object.keys(currScoreCategories).map((scoreCategory) => {
-						return (
-							<div key={"div_" + scoreCategory}>
-								<li key={"li_" + scoreCategory}> {scoreCategory}:
-									<ul>
-									{
-										currScoreCategories[scoreCategory].map((scoreName) => {
-											return (
-												<li key={"li_" + scoreName}> 
-													<Link to={"/" + scoreName}>{scoreName}</Link>
-												</li>
-											)
-										})
-									}
-									</ul>
-								</li>
-								<br />
-							</div>
-						)
-					})
-				}
-				</ul>
-			</nav>
-			
-			<Switch>
-			{
-				Object.keys(currScoreCategories).map((scoreCategory) => {
-					return currScoreCategories[scoreCategory].map((scoreName) => {
-						return (
-							<Route key={"route_" + scoreName} path={"/" + scoreName}>
-								<ScoreDisplay scoreName={scoreName} />
-							</Route>
-						)
-					})
-				})
-			}
-			</Switch>
-	</Router>
-	);
+                <h2> score list </h2>
+                <hr />
+                <ul>
+                {
+                    Object.keys(currScoreCategories).map((scoreCategory) => {
+                        return (
+                            <div key={"div_" + scoreCategory}>
+                                <li key={"li_" + scoreCategory}> {scoreCategory}:
+                                    <ul>
+                                    {
+                                        currScoreCategories[scoreCategory].map((scoreName) => {
+                                            return (
+                                                <li key={"li_" + scoreName}> 
+                                                    <Link to={"/" + scoreName}>{scoreName}</Link>
+                                                </li>
+                                            )
+                                        })
+                                    }
+                                    </ul>
+                                </li>
+                                <br />
+                            </div>
+                        )
+                    })
+                }
+                </ul>
+            </nav>
+            
+            <Switch>
+            {
+                Object.keys(currScoreCategories).map((scoreCategory) => {
+                    return currScoreCategories[scoreCategory].map((scoreName) => {
+                        return (
+                            <Route key={"route_" + scoreName} path={"/" + scoreName}>
+                                <ScoreDisplay scoreName={scoreName} />
+                            </Route>
+                        )
+                    })
+                })
+            }
+            </Switch>
+    </Router>
+    );
 }
 
 export {
-	ScoreRouter
+    ScoreRouter
 }
