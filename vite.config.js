590938209309e3b84400bf755dc7dd4c84cb7a9b
nicholas hung <nhung93@outlook.com>
2024-10-22 21:55:21 -0400

    switching webpack with vite

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+// https://stackoverflow.com/questions/74620427/how-to-configure-vite-to-allow-jsx-syntax-in-js-files
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    include: /\.js$/,
+    exclude: [],
+    loader: 'jsx',
+  },
+});
